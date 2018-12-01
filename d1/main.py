9ff755592cb01adc11e0f2b974ff2765848c2520
Jakub Potocki <kubac65@gmail.com>
2018-12-01 17:10:11 +0000

    Day 1 completed

---
+++ d1/main.py
@@ -0,0 +1,22 @@
+import urllib.request
+
+frequency = 0
+history = set()
+history.add(frequency)
+
+lines = []
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+continue_search = True
+while continue_search:
+    for l in lines:
+        drift = int(l)
+        frequency = frequency + drift
+        if frequency in history: # yeah we have a match
+            continue_search = False
+            break
+
+        history.add(frequency)
+else:
+    print('Frequency %d' % (frequency))
